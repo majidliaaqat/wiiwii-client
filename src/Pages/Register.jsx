a94dd3a9ee349891155a8e319309323fef56bd1d
Majid Liaqat <majidliaaqat@gmail.com>
2023-10-23 22:36:46 +0300

    setup for register

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "../styles/Register.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+
+
+export default Register;
